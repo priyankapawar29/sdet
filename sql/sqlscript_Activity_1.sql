4fd88f0fac30fdfa29f0e7724e026b88a6ff2d6e
Priyanka Pawar <49469215+priyankapawar29@users.noreply.github.com>
2021-01-13 11:19:18 +0530

    Sql Activity 1

---
+++ sql/sqlscript_Activity_1.sql
@@ -0,0 +1,8 @@
+REM   Script: SQL_Activity_1
+REM   SQL activity 1
+
+CREATE TABLE salesman (salesman_id int,salesman_name varchar2(20),salesman_city varchar2(20),commission int);
+
+desc salesman
+
+
